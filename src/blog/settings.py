7a428265422798936210356f50cb7d4faebf044c
keita02 <ansoumanekeita825@gmail.com>
2021-11-14 17:36:19 +0100

    database config

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
     
     'default' : {
 
-        'ENGINE'   : 'django.db.backends.postgresql',
+        'ENGINE'   : 'django.db.backends.mysql',
         'NAME'     : config('DATABASE_NAME'),
         'USER'     : config('DATABASE_USER') ,
         'PASSWORD' : config('DATABASE_PASSWORD') ,
